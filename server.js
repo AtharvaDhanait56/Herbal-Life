a1346a3d1c6135ff7ba381f4682dc1c9f44f445c
Atharva Dhanait <dhanaitatharva56@gmail.com>
2026-06-13 14:19:32 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // --- MYSQL DATABASE CONNECTION ---
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 db.connect((err) => {
